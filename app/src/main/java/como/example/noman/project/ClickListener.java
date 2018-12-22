1c35bc68bdc38225aea25a083f5b02f6a41b98ef
Wisaam <wisaam.arif.99@gmail.com>
2018-12-22 18:11:59 +0500

    Refactored ClickListener Code

---
+++ app/src/main/java/como/example/noman/project/ClickListener.java
@@ -0,0 +1,162 @@
+package como.example.noman.project;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.gson.Gson;
+import static android.content.Context.MODE_PRIVATE;
+
+public class ClickListener implements View.OnClickListener {
+
+    private Activity activity;  //use this activity or context when needed
+    private Context context;
+
+    ClickListener(Activity _activity, Context _context) //constructor
+    {
+        activity = _activity;
+        context = _context;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())              // add click function in this switch against the view id
+        {
+            case R.id.txt_already:
+                goToLogin();
+                break;
+            case R.id.create_account:
+                createAccount();
+                break;
+            case R.id.NoAccount:
+                goToRegister();
+                break;
+            case R.id.signinButton:
+                signin();
+                break;
+            default:
+                break;
+        }
+    }
+
+    //// Implement Click Functions Here ////
+
+    private void goToLogin() {
+        Intent intent = new Intent(activity, Login.class);
+        activity.startActivity(intent);
+    }
+
+    private void goToRegister() {
+        Intent register = new Intent(activity, Signup.class);
+        activity.startActivity(register);
+    }
+
+    private void createAccount() {
+        EditText UserName = (EditText) activity.findViewById(R.id.user_name);
+        EditText Email = (EditText) activity.findViewById(R.id.email_id);
+        EditText Password = (EditText) activity.findViewById(R.id.password);
+        EditText ConfirmPassword = (EditText) activity.findViewById(R.id.confirm_password);
+
+        if (UserName.getText().toString().trim().length() == 0) {
+            UserName.setError("Field cannot be left blank.");
+            return;
+        }
+
+        if (Email.getText().toString().trim().length() == 0) {
+            Email.setError("Field cannot be left blank.");
+            return;
+        }
+
+        if (Password.getText().length() == 0) {
+            Password.setError("Field cannot be left blank.");
+            return;
+        }
+
+        if (!ConfirmPassword.getText().toString().equals(Password.getText().toString())) {
+            ConfirmPassword.setError("Passwords don't match");
+            return;
+        }
+
+        if(!SaveData(UserName, Email, Password))
+        {
+            Email.setError("There is already an account associated with this email.");
+        }
+        else
+        {
+            Toast.makeText(activity, "Account Created!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean SaveData(EditText UserName, EditText Email, EditText Password) {
+        String email = Email.getText().toString().trim();
+        SharedPreferences mPrefs = activity.getSharedPreferences("info", MODE_PRIVATE);
+        String mail = mPrefs.getString(email, "");
+        if (mail != null && !mail.equals(""))
+        {
+            return false;
+        }
+
+        String Name = UserName.getText().toString();
+        String password = Password.getText().toString();
+
+
+        Person myObject = new Person(Name, email, password);  //creating new person object
+
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+
+        String json = gson.toJson(myObject);
+        prefsEditor.putString(email, json);
+
+        prefsEditor.apply();
+
+        return true;
+    }
+
+    private void signin() {
+        EditText emailAddress = activity.findViewById(R.id.emailText);
+        EditText passwordText = activity.findViewById(R.id.passText);
+
+        String mail = emailAddress.getText().toString().trim();
+        String p = passwordText.getText().toString();
+
+        if (mail.isEmpty())
+            emailAddress.setError("Enter your email!");
+        else if (p.isEmpty())
+            passwordText.setError("Enter your password!");
+
+        else
+        {
+            SharedPreferences mPrefs = activity.getSharedPreferences("info", MODE_PRIVATE);
+            Gson gson = new Gson();
+            String json = mPrefs.getString(mail, "none");
+            if(json != null && json.equals("none")) {
+                emailAddress.setError("Incorrect email!");
+                return;
+            }
+            if(json != null && !json.isEmpty()) {
+                Person obj = gson.fromJson(json, Person.class);
+                String password = obj.getPassword();
+
+                if (password.equals(p))
+                {
+                    Toast.makeText(activity, "switching Activity", Toast.LENGTH_LONG).show();
+
+                    //goto homapage after this
+
+                    //Intent homePage = new Intent(this, home.class);
+                    //startActivity(homePage);
+                }
+                else
+                {
+                    passwordText.setError("Incorrect Password!");
+                }
+            }
+
+        }
+
+    }
+}
--- app/src/main/java/como/example/noman/project/Signup.java
+++ app/src/main/java/como/example/noman/project/Signup.java
@@ -1,115 +1,28 @@
 package como.example.noman.project;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.gson.Gson;
-
-import java.io.File;
-import java.util.EnumMap;
-import java.util.Map;
-
-public class Signup extends AppCompatActivity
+public class Signup extends Activity
 {
-    EditText UserName;
-    EditText Email;
-    EditText Password;
-    EditText ConfirmPassword;
-    Button CreateAccount;
-    TextView Txt;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        UserName = (EditText) findViewById(R.id.user_name);
-        Email = (EditText) findViewById(R.id.email_id);
-        Password = (EditText) findViewById(R.id.password);
-        ConfirmPassword = (EditText) findViewById(R.id.confirm_password);
-        CreateAccount = (Button) findViewById(R.id.create_account);
-        Txt = (TextView) findViewById(R.id.txt_already);
+        EditText UserName = (EditText) findViewById(R.id.user_name);
+        EditText Email = (EditText) findViewById(R.id.email_id);
+        EditText Password = (EditText) findViewById(R.id.password);
+        EditText ConfirmPassword = (EditText) findViewById(R.id.confirm_password);
 
         UserName.setText("");
         Email.setText("");
         Password.setText("");
         ConfirmPassword.setText("");
 
-        Txt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
-            }
-        });
-
-        CreateAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (UserName.getText().toString().trim().length() == 0) {
-                    UserName.setError("Field cannot be left blank.");
-                    return;
-                }
-
-                if (Email.getText().toString().trim().length() == 0) {
-                    Email.setError("Field cannot be left blank.");
-                    return;
-                }
-
-                if (Password.getText().length() == 0) {
-                    Password.setError("Field cannot be left blank.");
-                    return;
-                }
-
-                if (ConfirmPassword.getText().length() == 0) {
-                    ConfirmPassword.setError("Confirm Password!.");
-                    return;
-                }
-
-                if(!SaveData(v))
-                {
-                    Email.setError("There is already an account associated with this email.");
-                }
-
-                //goto homepage after this
-            }
-        });
+        findViewById(R.id.txt_already).setOnClickListener(new ClickListener(this, getApplicationContext()));
+        findViewById(R.id.create_account).setOnClickListener(new ClickListener(this, getApplicationContext()));
     }
-
-    public boolean SaveData(View view)
-    {
-        String Name = UserName.getText().toString();
-        String email = Email.getText().toString();
-        String password = Password.getText().toString();
-
-        Person myObject = new Person(Name, email, password);  //creating new person object
-        SharedPreferences mPrefs = getSharedPreferences("info", MODE_PRIVATE);
-
-        String mail = mPrefs.getString(email, "");
-        if (!mail.equals(""))
-            return false;
-
-        SharedPreferences.Editor prefsEditor = mPrefs.edit();
-        Gson gson = new Gson();
-
-        String json = gson.toJson(myObject);
-        prefsEditor.putString(email, json);
-
-        prefsEditor.apply();
-
-        return true;
-    }
-
 }
 
