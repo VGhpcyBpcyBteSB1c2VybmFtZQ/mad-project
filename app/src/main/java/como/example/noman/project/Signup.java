ce48f4de6294512d7b2259aee8640da41c43eee7
Noman Afzal <noman.afzal@ucp.edu.pk>
2019-01-28 19:31:33 +0500

    Added_Profile_Page

--- app/src/main/java/como/example/noman/project/Signup.java
+++ app/src/main/java/como/example/noman/project/Signup.java
@@ -2,10 +2,14 @@ package como.example.noman.project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class Signup extends Activity
 {
+    static int ID = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +19,9 @@ public class Signup extends Activity
         EditText Email = (EditText) findViewById(R.id.email_id);
         EditText Password = (EditText) findViewById(R.id.password);
         EditText ConfirmPassword = (EditText) findViewById(R.id.confirm_password);
+        CheckBox CheckBox = findViewById(R.id.signup_admin_checkbox);
+        EditText Cnic = findViewById(R.id.cnic);
+        EditText Phone = findViewById(R.id.Phone);
 
         UserName.setText("");
         Email.setText("");
@@ -23,6 +30,7 @@ public class Signup extends Activity
 
         findViewById(R.id.txt_already).setOnClickListener(new ClickListener(this, getApplicationContext()));
         findViewById(R.id.create_account).setOnClickListener(new ClickListener(this, getApplicationContext()));
+        findViewById(R.id.signup_admin_checkbox).setOnClickListener(new ClickListener(this, getApplicationContext()));
         findViewById(R.id.signup_GoToHome).setOnClickListener(new ClickListener(this, getApplicationContext()));
     }
 
