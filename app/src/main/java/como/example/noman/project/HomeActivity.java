7f2787d9a0207ce64ad0caa8ade54ff410d99dd0
Hashir Umar <32617270+Hashir-Umar@users.noreply.github.com>
2019-02-19 13:21:48 -0800

    updated toolbar icon + updated size of home icon

--- app/src/main/java/como/example/noman/project/HomeActivity.java
+++ app/src/main/java/como/example/noman/project/HomeActivity.java
@@ -224,7 +224,6 @@ public class HomeActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
